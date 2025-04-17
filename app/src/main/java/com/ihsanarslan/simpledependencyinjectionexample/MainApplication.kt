3d807e6cdf5cdd7867314b317d5db74fe5152289
mrihsann <thearslanihsan@gmail.com>
2025-04-17 17:44:29 +0300

    Enable Hilt for dependency injection
    This commit configures the application to use Hilt for dependency injection by:
    -   Adding the `@HiltAndroidApp` annotation to the `MainApplication` class.
    -   Specifying `MainApplication` as the application name in `AndroidManifest.xml`.

---
+++ app/src/main/java/com/ihsanarslan/simpledependencyinjectionexample/MainApplication.kt
@@ -0,0 +1,7 @@
+package com.ihsanarslan.simpledependencyinjectionexample
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication : Application()
