4c9b993c028650aa264734292a03c4f0585a9d14
mrihsann <thearslanihsan@gmail.com>
2025-04-17 19:33:59 +0300

    Refactor MainActivity to use MyScreen
    This commit refactors `MainActivity` to display `MyScreen` instead of a simple greeting. It removes the `Greeting` composable and its preview, and replaces the content of `MainActivity` with `MyScreen`.

--- app/src/main/java/com/ihsanarslan/simpledependencyinjectionexample/MainActivity.kt
+++ app/src/main/java/com/ihsanarslan/simpledependencyinjectionexample/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.ihsanarslan.simpledependencyinjectionexample.presentation.MyScreen
 import com.ihsanarslan.simpledependencyinjectionexample.ui.theme.SimpleDependencyInjectionExampleTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,29 +15,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SimpleDependencyInjectionExampleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SimpleDependencyInjectionExampleTheme {
-        Greeting("Android")
     }
 }
